3db07b29959c9e625b7dbcfa1b60ea85da945bce
101 <101@CLASS101_17>
2019-10-19 20:14:12 +0500

    add GUI objects

--- bank-account/Form1.cs
+++ bank-account/Form1.cs
@@ -12,9 +12,12 @@ namespace bank_account
 {
     public partial class Form1 : Form
     {
+        public string[] AccountTypes = new[] { "Банк Акаунт", "Блокчейн Акаунт" };
         public Form1()
         {
             InitializeComponent();
+            //var AccountTypes = new[] { "Банк Акаунт", "Блокчейн Акаунт" };
+            comboBox1.DataSource = AccountTypes;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -24,12 +27,26 @@ namespace bank_account
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            int currentcomboBox1Index = comboBox1.SelectedIndex;
+            switch (currentcomboBox1Index)
+            {
+                case 0:
+                    label1.Text = AccountTypes[0];
+                    break;
+                case 1:
+                    label1.Text = AccountTypes[1];
+                    break;
+            }
+            //label1.Text = "Hello";
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
         }
     }
 }
